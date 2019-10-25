2d466efcb5d1ea616645828a72893b59f5684c59
丁丁历险记 <james.song007@hotmail.com>
2019-10-25 16:34:48 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="login">
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary" @click="ccc">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <router-view />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+const log = '【登录页面】'
+export default {
+  data () {
+    return {
+      active: 0,
+      aaa: 'sdf水电费'
+    }
+  },
+  mounted () {
+    let name = '【打印mounted函数】'
+    console.info(log, name)
+  },
+  methods: {
+    ccc () {
+      console.info(this.aaa)
+      this.$router.push({
+        name: 'HelloWorld'
+      })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/a',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }, {
+    path: '/',
+    name: 'Login',
+    component: Login
+  }]
+})
