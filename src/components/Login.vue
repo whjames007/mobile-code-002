94b6934ec258db5e37967b58a6c19affeb4536fd
丁丁历险记 <james.song007@hotmail.com>
2019-10-28 08:55:31 +0800

    d

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,7 +14,6 @@
           right-icon='question-o'
           placeholder='请输入用户名'
         />
-
         <van-field v-model='password' type='password' label='密码' placeholder='请输入密码' required />
       </van-cell-group>
     </div>
